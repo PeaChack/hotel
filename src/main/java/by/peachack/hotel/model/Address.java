93dcd066939718a24a6a3b154706cbdddacf8dc6
PeaChack <dev.peachack@gmail.com>
2024-06-06 12:22:13 +0300

    add constructor annotation to entities

--- src/main/java/by/peachack/hotel/model/Address.java
+++ src/main/java/by/peachack/hotel/model/Address.java
@@ -2,10 +2,14 @@ package by.peachack.hotel.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
     @Column(name = "house_number")
     private int houseNumber;
--- src/main/java/by/peachack/hotel/model/Amenity.java
+++ src/main/java/by/peachack/hotel/model/Amenity.java
@@ -1,18 +1,24 @@
 package by.peachack.hotel.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "amenity")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Amenity {
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "name")
     private String name;
+
+    public Amenity(String s) {
+        this(null, s);
+    }
 }
