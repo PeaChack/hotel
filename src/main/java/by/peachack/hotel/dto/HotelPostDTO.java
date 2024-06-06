da1c069b7c2d1118564799d3eef74fd9f2f4d902
PeaChack <dev.peachack@gmail.com>
2024-06-06 12:23:49 +0300

    add HotelPostDTO, change HotelDetailedDTO, according to requirements

--- src/main/java/by/peachack/hotel/dto/HotelPostDTO.java
+++ src/main/java/by/peachack/hotel/dto/HotelPostDTO.java
@@ -3,17 +3,18 @@ package by.peachack.hotel.dto;
 import by.peachack.hotel.model.Address;
 import by.peachack.hotel.model.ArrivalTime;
 import by.peachack.hotel.model.Contacts;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
+import lombok.NoArgsConstructor;
 
 @Data
-public class HotelDetailedDTO {
-    private int id;
+@AllArgsConstructor
+@NoArgsConstructor
+public class HotelPostDTO {
     private String name;
+    private String description;
     private String brand;
     private Address address;
     private Contacts contacts;
     private ArrivalTime arrivalTime;
-    private List<String> amenities;
 }
