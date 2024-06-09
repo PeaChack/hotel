587a2750e43f774dae2c4bd4ebdcdef520999239
PeaChack <dev.peachack@gmail.com>
2024-06-09 15:10:53 +0300

    add Valid annotations to contacts, address and arrival time

--- src/main/java/by/peachack/hotel/dto/HotelPostDTO.java
+++ src/main/java/by/peachack/hotel/dto/HotelPostDTO.java
@@ -3,6 +3,7 @@ package by.peachack.hotel.dto;
 import by.peachack.hotel.model.Address;
 import by.peachack.hotel.model.ArrivalTime;
 import by.peachack.hotel.model.Contacts;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -23,9 +24,12 @@ public class HotelPostDTO {
     @Size(max = 100, message = "Brand cannot be longer than 100 characters")
     private String brand;
     @NotNull(message = "Address cannot be null")
+    @Valid
     private Address address;
     @NotNull(message = "Contacts cannot be null")
+    @Valid
     private Contacts contacts;
     @NotNull(message = "Arrival time cannot be null")
+    @Valid
     private ArrivalTime arrivalTime;
 }
